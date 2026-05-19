2c8eff08eefc49b6d9d94808af711ed69a64751f
aramirez-neural <aramirez@neural.es>
2026-05-19 15:45:57 +0200

    haciendo el botón y la parte de la música.

---
+++ src/components/common/MuteButton.tsx
@@ -0,0 +1,8 @@
+
+export default function MuteButton() {
+  return (
+    <button type='button' className="fixed bottom-5 right-5 h-15 w-15 rounded-full bg-theme-red">
+      
+    </button>
+  )
+}
